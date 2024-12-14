ee02b374428cb6073c40c0694bc1b77e1ba9dcd4
jkanbi <jkanbi@users.noreply.github.com>
2024-12-14 00:09:14 +0000

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -3,10 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const navbarHtml = `
         <nav class="navbar">
             <ul class="nav-list">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="contact.html">Contact</a></li>
+
+                <li><a href="/">Home</a></li>
+                <li><a href="index.html">Boilers</a></li>
+                <li><a href="controls.html">Controls</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+
             </ul>
         </nav>
     `;
