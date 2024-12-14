ecf8a34f90f3951e0729e2887580ce8c786ae71c
jkanbi <jkanbi@users.noreply.github.com>
2024-12-14 18:25:37 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,3 +34,35 @@ function myFunction() {
                 }
             }
         }
+
+function myTableFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, j, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    // Get all cells in the current row
+    td = tr[i].getElementsByTagName("td");
+    let matchFound = false;
+
+    // Check each cell in the row
+    for (j = 0; j < td.length; j++) {
+      txtValue = td[j].textContent || td[j].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        matchFound = true;
+        break; // If match is found, no need to check further cells in this row
+      }
+    }
+
+    // Show or hide the row based on if a match was found
+    if (matchFound) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
+    }
+  }
+}
