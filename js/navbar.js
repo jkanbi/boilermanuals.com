b403776f0e595743ab4af95ca452a8c694c3950a
jkanbi <jkanbi@users.noreply.github.com>
2026-06-09 15:49:12 +0100

    refactor code/pages to use templated navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,42 +1,110 @@
-// navbar.js
-document.addEventListener('DOMContentLoaded', function() {
-    if (document.querySelector('.navbar')) {
-        return;
-    }
+(function () {
+    var NAV_ITEMS = [
+        { id: "boilers", label: "Boilers", href: "index.html" },
+        { id: "fault-codes", label: "Fault Codes", href: "fault-codes.html" },
+        { id: "controls", label: "Controls", href: "controls.html" },
+        { id: "contact", label: "Contact", href: "contact.html" },
+        { id: "about", label: "About", href: "about.html" },
+    ];
 
-    var basePath = getNavbarBasePath();
-
-    var navbarHtml = ''
-        + '<nav class="navbar">'
-        + '  <a href="' + basePath + 'index.html" class="navbar-brand">'
-        + '    <img src="' + basePath + 'images/logo.png" alt="" class="navbar-logo" width="44" height="44">'
-        + '    <span class="navbar-title">Boiler Manuals</span>'
-        + '  </a>'
-        + '  <div class="navbar-actions">'
-        + '    <button class="navbar-toggle" onclick="toggleMenu()" aria-label="Open menu">'
-        + '      <i class="fas fa-bars"></i>'
-        + '    </button>'
-        + '    <ul class="navbar-menu">'
-        + '      <li><a href="' + basePath + 'index.html">Boilers</a></li>'
-        + '      <li><a href="' + basePath + 'controls.html">Controls</a></li>'
-        + '      <li><a href="' + basePath + 'contact.html">Contact</a></li>'
-        + '      <li><a href="' + basePath + 'about.html">About</a></li>'
-        + '    </ul>'
-        + '  </div>'
-        + '</nav>';
-
-    document.body.insertAdjacentHTML('afterbegin', navbarHtml);
-});
-
-function getNavbarBasePath() {
-    var scripts = document.getElementsByTagName('script');
-
-    for (var i = 0; i < scripts.length; i++) {
-        var src = scripts[i].getAttribute('src');
-        if (src && src.indexOf('navbar.js') !== -1) {
-            return src.replace(/js\/navbar\.js(?:\?.*)?$/, '');
+    function getNavbarBasePath() {
+        var scripts = document.getElementsByTagName("script");
+        for (var i = 0; i < scripts.length; i++) {
+            var src = scripts[i].getAttribute("src") || "";
+            if (src.indexOf("navbar.js") !== -1) {
+                return src.replace(/js\/navbar\.js(?:\?.*)?$/, "");
+            }
         }
+        return "";
     }
 
-    return '';
-}
+    function getActiveItemId() {
+        var page = window.location.pathname.split("/").pop() || "index.html";
+        if (!page || page === "/") {
+            page = "index.html";
+        }
+        for (var i = 0; i < NAV_ITEMS.length; i++) {
+            if (NAV_ITEMS[i].href === page) {
+                return NAV_ITEMS[i].id;
+            }
+        }
+        return "";
+    }
+
+    function escapeAttr(value) {
+        return String(value)
+            .replace(/&/g, "&amp;")
+            .replace(/"/g, "&quot;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;");
+    }
+
+    function renderNavbar() {
+        if (document.querySelector(".navbar")) {
+            return;
+        }
+
+        var basePath = getNavbarBasePath();
+        var activeId = getActiveItemId();
+        var menuHtml = "";
+
+        for (var i = 0; i < NAV_ITEMS.length; i++) {
+            var item = NAV_ITEMS[i];
+            var current = item.id === activeId ? ' aria-current="page"' : "";
+            menuHtml +=
+                '<li><a href="' +
+                escapeAttr(basePath + item.href) +
+                '"' +
+                current +
+                ">" +
+                item.label +
+                "</a></li>";
+        }
+
+        var navbarHtml =
+            '<nav class="navbar">' +
+            '<a href="' +
+            escapeAttr(basePath + "index.html") +
+            '" class="navbar-brand">' +
+            '<img src="' +
+            escapeAttr(basePath + "images/logo.png") +
+            '" alt="" class="navbar-logo" width="44" height="44">' +
+            '<span class="navbar-title">Boiler Manuals</span>' +
+            "</a>" +
+            '<div class="navbar-actions">' +
+            '<button class="navbar-toggle" onclick="toggleMenu()" aria-label="Open menu">' +
+            '<i class="fas fa-bars"></i>' +
+            "</button>" +
+            '<ul class="navbar-menu">' +
+            menuHtml +
+            "</ul>" +
+            "</div>" +
+            "</nav>";
+
+        document.body.insertAdjacentHTML("afterbegin", navbarHtml);
+    }
+
+    window.toggleMenu = function toggleMenu() {
+        var menu = document.querySelector(".navbar-menu");
+        var icon = document.querySelector(".navbar-toggle i");
+        if (!menu || !icon) {
+            return;
+        }
+
+        menu.classList.toggle("active");
+
+        if (menu.classList.contains("active")) {
+            icon.classList.remove("fa-bars");
+            icon.classList.add("fa-times");
+        } else {
+            icon.classList.remove("fa-times");
+            icon.classList.add("fa-bars");
+        }
+    };
+
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", renderNavbar);
+    } else {
+        renderNavbar();
+    }
+})();
